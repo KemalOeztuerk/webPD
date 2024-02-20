e8ad93c1013cf82673d4509a1313423e18a2eda9
root <root@debian.debian>
2024-02-20 20:21:41 +0300

    audio_schema modified, homepage modified

--- AudioService/src/api/schemas/audio_schema.py
+++ AudioService/src/api/schemas/audio_schema.py
@@ -5,8 +5,4 @@ ma = Marshmallow()
 
 class AudioSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'bpm')
-    _links = ma.Hyperlinks({
-        'self': ma.URLFor('get_user', id='<id>'),
-        'collection': ma.URLFor('get_users')
-    })
+        fields = ('id', 'name', 'bpm', 'length')
